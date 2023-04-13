e874f660e559674adb280a0d60e5b4a01fdd8501
Bill <Bill@TheWallisFamily.co.uk>
2023-04-13 22:07:26 +0100

    ✨ feat: Add files for Google Sheets

---
+++ Google-Sheets/sheets.py
@@ -0,0 +1,6 @@
+"""
+Following the instructions at:
+
+- https://developers.google.com/sheets/api/quickstart/python
+- https://github.com/googleworkspace/python-samples/blob/main/sheets/quickstart/quickstart.py
+"""
