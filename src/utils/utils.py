3d9fd2f6756097f1d1d28e5b5e400043101f4e69
Bill <bill@thewallisfamily.co.uk>
2025-03-02 12:51:14 +0000

    feat: refine utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,8 +4,11 @@ from rich import print
 
 
 def pprint(text: str | dict, indent: int = 4) -> None:
-    """Pretty print JSON/dict objects"""
-    if type(text) is str:
+    """
+    Pretty print JSON/dict objects.
+    """
+
+    if isinstance(text, str):
         text = json.loads(text)
 
     try:
@@ -14,7 +17,7 @@ def pprint(text: str | dict, indent: int = 4) -> None:
                 text,
                 sort_keys=True,
                 indent=indent,
-                separators=(",", ": ")
+                separators=(",", ": "),
             )
         )
     except TypeError:
@@ -35,16 +38,19 @@ def print_dict_types(dictionary: dict, level: int = 0) -> None:
             print_dict_types(value, level=level + 1)
 
 
-def json_schema_to_sql() -> None:
+def _json_schema_to_sql() -> None:
     """
     Loop through a dict and convert to SQL column names with types.
     """
+
     def printer(dictionary: dict, parent: str or None = None) -> None:
         for key, value in dictionary.items():
             if isinstance(value, dict):
                 printer(value, parent=key)
             else:
-                print(f"{key if parent is None else f'{parent}_{key}'} {sql_type[type(value)]},")
+                print(
+                    f"{key if parent is None else f'{parent}_{key}'} {sql_type[type(value)]},"
+                )
 
     sql_type = {
         str: "TEXT",
@@ -52,7 +58,7 @@ def json_schema_to_sql() -> None:
         dict: "BLOB",
         int: "INTEGER",
         bool: "INTEGER  /* BOOL */",
-        float: "REAL"
+        float: "REAL",
     }
     with open("temp.json") as f:
         printer(json.loads(f.read()))
--- utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Handy utilities for working with (REST) API data"""
-from utils.utils import (
-    pprint,
-    print_dict_types
-)
