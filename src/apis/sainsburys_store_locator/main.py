da17a6eca1eee61f2b2d9473df76d1c3a48bb317
Bill <bill@thewallisfamily.co.uk>
2025-03-08 10:04:01 +0000

    feat: add Sainsbury's Store Locator connector

---
+++ src/apis/sainsburys_store_locator/main.py
@@ -0,0 +1,52 @@
+"""
+Manual testing for the API clients.
+"""
+
+import json
+import pathlib
+
+import src.utils
+from src.apis.sainsburys_store_locator import StoreLocatorConnector
+
+# The public API is limited to 20 results per request
+CHUNK_SIZE = 20
+
+
+def get_all_stores(store_locator: StoreLocatorConnector) -> list[dict]:
+    """
+    Get all stores from the API by making multiple requests.
+    """
+
+    stores, results = [], -1
+    while results != 0:
+        print("•", end="")
+        params = {
+            "limit": CHUNK_SIZE,
+            "offset": len(stores),
+            "fields": "closed,code,contact,location,name,other_name,store_type",
+        }
+        response = store_locator.get_stores(params).json()["results"]
+        results = len(response)
+        stores.extend(response)
+    print()
+
+    return stores
+
+
+def main() -> None:
+    """
+    Manually test the API client.
+    """
+    store_locator = StoreLocatorConnector()
+
+    src.utils.pprint(store_locator.get_stores().json())
+    src.utils.pprint(store_locator.get_store_by_id(store_id=2).json())
+
+    all_stores = get_all_stores(store_locator)
+    print(len(all_stores))
+    f = pathlib.Path("data/sainsburys-stores.json")
+    f.write_text(json.dumps(all_stores), encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()
